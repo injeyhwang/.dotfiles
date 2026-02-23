64d8cf1db6aa5e9d3d76582a110d3488c1ed56d0
Michael Hwang <michaelinjeyhwang@gmail.com>
2026-02-23 17:24:06 +0000

    match LazyVim's autocmds defaults

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,11 +1,131 @@
 -- [[ Basic Autocommands ]]
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("config_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed outside of nvim
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  desc = "Check if we need to reload the file",
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- Restore cursor to last known location when opening a file
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Restore last cursor location",
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].config_last_loc then
+      return
+    end
+    vim.b[buf].config_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Resize splits when the Neovim window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+  desc = "Resize splits equally when window is resized",
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- Close some special buffers with "q"
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Close some filetypes with <q>",
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, { buffer = event.buf, silent = true, desc = "Quit buffer" })
+    end)
+  end,
+})
+
+-- Make man pages unlisted when opened inline
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Unlist man buffers",
+  group = augroup("man_unlisted"),
+  pattern = { "man" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+  end,
+})
+
+-- Enable wrapping and spell checking for writing filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Enable wrap and spell for writing filetypes",
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Disable conceal for json filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Disable conceal for json filetypes",
+  group = augroup("json_conceal"),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+-- Auto-create missing parent directories when saving files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Auto create missing parent directories on save",
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  desc = "Highlight when yanking text",
+  group = augroup("highlight_yank"),
   callback = function()
     vim.highlight.on_yank()
   end
 })
-
