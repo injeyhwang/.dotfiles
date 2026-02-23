9f850050b3dcc9c03af5e2806f07fb544aa94d0b
Michael Hwang <michaelinjeyhwang@gmail.com>
2026-02-23 18:53:26 +0000

    show-key is now positioned top-right and has keymaps

--- .config/nvim/lua/plugins/showkeys.lua
+++ .config/nvim/lua/plugins/showkeys.lua
@@ -2,12 +2,19 @@ return {
   "nvzone/showkeys",
   event = "VeryLazy",
   cmd = "ShowkeysToggle",
+  keys = {
+    {
+      "<leader>uK",
+      "<cmd>ShowkeysToggle<cr>",
+      desc = "Toggle Showkeys",
+    },
+  },
   opts = {
     timeout = 3,
     maxkeys = 5,
+    position = "top-right",
   },
   config = function(_, opts)
     require("showkeys").setup(opts)
-    require("showkeys").toggle()
   end
 }
