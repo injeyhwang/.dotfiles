ce3a8920d6f34284e7cd011cb288faeb56db1aea
Michael Hwang <mih2122@icloud.com>
2026-08-23 14:56:31 -0400

    improve cursor position restore to handle all windows showing the buffer

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -29,7 +29,9 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      for _, window in ipairs(vim.fn.win_findbuf(buf)) do
+        pcall(vim.api.nvim_win_set_cursor, window, mark)
+      end
     end
   end,
 })
